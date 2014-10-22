f506f8cecbae00b34a31bcdb6357b4c0ffc9621f
Jaufré Devosse <djaufre@gmail.com>
2014-10-22 09:39:18 +0200

    fix (core):Change the message printed in the commit hook

--- git-hooks/commit-msg.pl
+++ git-hooks/commit-msg.pl
@@ -32,14 +32,10 @@ $data .= "\n";
 
 $pattern = '\A(' . join("|", @types) . ') \((' . join("|", @scopes) . ')\):[^ ].{10,100}';
 
-print("\n" . '$pattern : [' . $pattern . "]\n");
-
-print ('$data' . $data);
-
 if ($data !~ m/$pattern/simg) {
 	print("The commit message is not well formed.\n");
 	print("It must be like this : \n");
-	print("\t\ttype (scope) : <subject>\n\t\t\n\t\t[body]\n\t\t\n\t\t[WorkItem : #XXXXX]");
+	print("\t\ttype (scope):<subject>\n\t\t\n\t\t[body]\n\t\t\n\t\t[WorkItem : #XXXXX]");
 	print("\n\nType can be : " . join(", ", @types));
 	print("\nScope can be : " . join(", ", @scopes));
 	print("\nSubject can not be longer than  100 characters!");
